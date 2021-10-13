4c11efaeff706eacd95fcace7c10ac80903d60ff
ltphuong1110 <92436179+ltphuong1110@users.noreply.github.com>
2021-10-13 17:24:16 +0700

    upload code

---
+++ README.md
@@ -0,0 +1,3 @@
+# Happy Birthday bé Phươngggggggg
+## _Hy vọng là 1 món quà đặc biệt dành tặng :>_
+
---
+++ config.js
@@ -0,0 +1,16 @@
+const CONFIG = {
+    introTitle: 'Helu Baeeeeeeee',
+    introDesc: `Hôm nay ngày 11/10/2021 và hôm nay bé đã được 22 chủi.`,
+    btnIntro: 'hihihihihihihihi',
+    title: 'Happy Birthday Bé iuuuu 😘😘',
+    desc: 'From Bé Trí with loveeeeee 😘😘',
+    btnYes: 'Muốn tiếp tục <33',
+    btnNo: 'Nghỉ hoyyyy :<',
+    question:'Ủa sao 2 mình biết nhao dạaaaa. ',
+    btnReply: 'Gửi cho bé nà <3',
+    reply: 'chắc do 2 mình có duyên áaaaaa hihihihihi',
+    mess: 'Biết oyyyy 🥰. Thương bé êu nhều 😘😘',
+    messDesc: 'Chiều chiều bé sẽ mua gì mang qua cho nhennnnnnn qua gọi. Chúc bé có 1 sinh nhật thật hạnh phúc',
+    btnAccept: 'Okiiiii lun, check messenger nèo <3',
+    messLink: 'http://fb.com/tristana.tran1507' 
+}
